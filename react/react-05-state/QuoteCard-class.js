971c84d97df7a694c07d6e2b63c939fb4760605d
Benoît Hubert <benoithubert@gmail.com>
2019-10-09 09:55:46 +0200

    Solutions pour la quête state

---
+++ react/react-05-state/QuoteCard-class.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './QuoteCard.css';
+
+class QuoteCard extends React.Component {
+  render() {
+    return (
+      <figure className="QuoteCard">
+        <img src={this.props.image} alt={this.props.character} />
+        <figcaption>
+          <blockquote>{this.props.quote}</blockquote>
+          <cite>{this.props.character}</cite>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+export default QuoteCard;
+
