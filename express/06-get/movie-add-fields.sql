5267b5e679aafbee67243555da69467a95652edb
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2019-12-03 09:41:27 +0100

    Update movie-add-fields.sql

--- express/06-get/movie-add-fields.sql
+++ express/06-get/movie-add-fields.sql
@@ -1,8 +1,8 @@
 ALTER TABLE movie ADD COLUMN rating SMALLINT;
-ALTER TABLE movie ADD COLUMN category ENUM('Comedy', 'Drama', 'Romance', 'Fantasy');
+ALTER TABLE movie ADD COLUMN genre ENUM('Comedy', 'Drama', 'Romance', 'Fantasy');
 
 UPDATE movie SET rating = FLOOR(RAND()*6);
-UPDATE movie SET category = CASE
+UPDATE movie SET genre = CASE
   WHEN RAND() < 0.25 THEN 'Comedy'
   WHEN RAND() < 0.33 THEN 'Drama'
   WHEN RAND() < 0.5 THEN 'Romance'
